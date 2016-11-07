ead1f8635a8d773dc00ef507a41315ff0ea0db3a
Martin Vega <mvegaca@outlook.com>
2016-11-07 10:36:45 +0100

    Set App Name to Publish

--- Codemotion2016/WorkshopApp/Properties/AssemblyInfo.cs
+++ Codemotion2016/WorkshopApp/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WorkshopApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Simply WordPress")]
+[assembly: AssemblyDescription("Simply WordPress application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WorkshopApp")]
@@ -27,3 +28,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
+
