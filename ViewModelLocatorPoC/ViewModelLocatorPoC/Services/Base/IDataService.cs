d26979fed18a3a1e51495b228c1ad67d065a61f7
Martin Vega <mvegaca@outlook.com>
2017-01-10 17:48:57 +0100

    Add ViewModelLocatorPoC

---
+++ ViewModelLocatorPoC/ViewModelLocatorPoC/Services/Base/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using AppStudio.DataProviders.Rss;
+
+namespace ViewModelLocatorPoC
+{
+    public interface IDataService
+    {
+        Task<IEnumerable<RssSchema>> GetData();
+    }
+}
